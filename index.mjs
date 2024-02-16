6e1b20a4a9fddde273865a7853968a51918702c9
Alex Antia <agjantia@gmail.com>
2024-02-16 16:27:17 +0000

    more messing about

--- index.mjs
+++ index.mjs
@@ -1,26 +1,35 @@
-import { KaminoMarket } from "@hubbleprotocol/kamino-lending-sdk";
 import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
+
+import Kamino from "@hubbleprotocol/kamino-lending-sdk";
 
 const connection = new Connection(clusterApiUrl('mainnet-beta'));
 console.log("ping")
+const exampleKey = new PublicKey("7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF")
+console.log(await connection.getBalance(exampleKey))//connection works
 
-// There are three levels of data you can request (and cache) about the lending market.
-// 1. Initalize market with parameters and metadata
-const market = await KaminoMarket.load(
-    connection,
-    new PublicKey("7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF") // main market address. Defaults to 'Main' market
-);
-console.log(market.reserves.map((reserve) => reserve.config.loanToValueRatio));
+const markets = await Kamino.getMarketsFromApi()
+console.log(markets)
+const market = await Kamino.KaminoReserve.load(connection, new PublicKey("EAA3VVsxUuQB1Tm5x7TJkq9ATtiX5Qwq8ok7gXwim7oo"))
+console.log(market)
+//const reserve = await Kamino.KaminoMarket.prototype.getReservesForMarket
 
-// 2. Refresh reserves
-await market.loadReserves();
+// // There are three levels of data you can request (and cache) about the lending market.
+// // 1. Initalize market with parameters and metadata
+// const market = await KaminoMarket.load(
+//     connection,
+//     new PublicKey("7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF") // main market address. Defaults to 'Main' market
+// );
+// console.log(market.reserves.map((reserve) => reserve.config.loanToValueRatio));
 
-const usdcReserve = market.getReserve("USDC");
-console.log(usdcReserve?.stats.totalDepositsWads.toString());
+// // 2. Refresh reserves
+// await market.loadReserves();
+
+// const usdcReserve = market.getReserve("USDC");
+// console.log(usdcReserve?.stats.totalDepositsWads.toString());
 
 
-// Refresh all cached data
-market.refreshAll();
+// // Refresh all cached data
+// market.refreshAll();
 
-//   const obligation = market.getObligationByWallet("WALLET_PK");
-//   console.log(obligation.stats.borrowLimit);
+// //   const obligation = market.getObligationByWallet("WALLET_PK");
+// //   console.log(obligation.stats.borrowLimit);
