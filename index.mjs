0d32f5b041ba83007d83191470db700b1980b9af
Alex Antia <agjantia@gmail.com>
2024-02-16 16:48:06 +0000

    abandoned sdk approach

--- index.mjs
+++ index.mjs
@@ -1,16 +1,25 @@
 import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
 
-import Kamino from "@hubbleprotocol/kamino-lending-sdk";
+// import Kamino from "@hubbleprotocol/kamino-lending-sdk";
 
+// console.log(clusterApiUrl('mainnet-beta'))
 const connection = new Connection(clusterApiUrl('mainnet-beta'));
-console.log("ping")
-const exampleKey = new PublicKey("7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF")
+// console.log("ping")
+// const exampleKey = new PublicKey("7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF")
 console.log(await connection.getBalance(exampleKey))//connection works
+// //7dD6bhVgWtxyQPDAuM6Vprd22K7H6xzDYdsUbg9B3KUC // jlp holding account
+// const markets = await Kamino.getMarketsFromApi()
+// console.log(markets)
+// //const market = await Kamino.KaminoMarket.load(connection, new PublicKey("7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF"))
+// //console.log(market)
 
-const markets = await Kamino.getMarketsFromApi()
-console.log(markets)
-const market = await Kamino.KaminoReserve.load(connection, new PublicKey("EAA3VVsxUuQB1Tm5x7TJkq9ATtiX5Qwq8ok7gXwim7oo"))
-console.log(market)
+const jlpAccount = new PublicKey("7dD6bhVgWtxyQPDAuM6Vprd22K7H6xzDYdsUbg9B3KUC")
+const accountBalance = await connection.getTokenAccountBalance(jlpAccount)
+console.log(accountBalance)
+
+// if accountbalance.value.uiAmount > 8000000, use the microsoft voice API to say "KAMINO KAMINO KAMINO"
+
+
 //const reserve = await Kamino.KaminoMarket.prototype.getReservesForMarket
 
 // // There are three levels of data you can request (and cache) about the lending market.
